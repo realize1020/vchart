e3e913da0a064fa93af468000bbef5e21404f5de
realize1020 <46668485+realize1020@users.noreply.github.com>
2023-06-09 09:46:48 +0800

    2、适应后端添加springsecurity框架，前端增加反向代理解决跨域问题

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 
-
-
 Vue.use(VCharts)
 Vue.use(ElementUI)
 Vue.config.productionTip = false
